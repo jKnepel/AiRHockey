fb2c9fd6e6da7b6199f9703f38438d9fe3229147
jKnepel <knepel.julian@tutanota.com>
2023-06-26 17:05:10 +0200

    feat: added outline, cleanup

--- Assets/Scripts/Runtime/AiRHockey/UI/JoinGameMenu.cs
+++ Assets/Scripts/Runtime/AiRHockey/UI/JoinGameMenu.cs
@@ -11,7 +11,7 @@ namespace HTW.AiRHockey.UI
 {
     public class JoinGameMenu : MonoBehaviour
     {
-        private const int UPDATE_LOBBIES_TIMEOUT = 5000;
+        [SerializeField] private const int UPDATE_LOBBIES_TIMEOUT = 5000;
 
         [SerializeField] private GameObject lobbyPrefab;
         [SerializeField] private GameObject container;
