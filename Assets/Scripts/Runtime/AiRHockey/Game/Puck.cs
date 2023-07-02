85ae14d3375e58ac94646c6bebb558bbde4d4e56
Tim Oelkers <tim.oelkers@web.de>
2023-07-02 18:14:13 +0200

    feat: add puck sound effects

--- Assets/Scripts/Runtime/AiRHockey/Game/Puck.cs
+++ Assets/Scripts/Runtime/AiRHockey/Game/Puck.cs
@@ -1,15 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace HTW.AiRHockey.Game
 {
-	[RequireComponent(typeof(Rigidbody), typeof(AudioSource))]
+	[RequireComponent(typeof(Rigidbody))]
     public class Puck : MonoBehaviour
     {
+		[SerializeField] private AudioSource _puckPlayerSound;
+		[SerializeField] private AudioSource _puckBoardSound;
+
 		private void OnCollisionEnter(Collision collision)
 		{
-			// TODO : player collision sound
+			Debug.Log(collision.gameObject.layer);
+			if (collision.gameObject.layer == LayerMask.NameToLayer("Player"))
+			{
+				_puckPlayerSound.Play();
+			} 
+			else if (collision.gameObject.layer == LayerMask.NameToLayer("Barrier"))
+			{
+				_puckBoardSound.Play();
+			}
 		}
 
 		private void OnTriggerEnter(Collider other)
