d36c381fe14727c4f968edb1e808b365a5a4fea9
Tim Oelkers <tim.oelkers@web.de>
2023-07-02 22:19:29 +0200

    feat: basic end game screen

---
+++ Assets/Scripts/Runtime/AiRHockey/UI/WinScreen.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using HTW.AiRHockey.Game;
+using TMPro;
+
+namespace HTW.AiRHockey.UI
+{
+    public class WinScreen : MonoBehaviour
+    {
+        private void Start()
+        {
+            GameManagerEvents.OnGameWon += GameWon;
+        }
+
+        private void OnDestroy()
+        {
+            GameManagerEvents.OnGameWon -= GameWon;
+        }
+
+        public void OnRematchClick()
+        {
+            InstanceFinder.GameManager.ReadyUp();
+            gameObject.SetActive(false);
+        }
+
+        public void OnLeaveClick()
+        {
+            InstanceFinder.GameManager.DisconnectFromServer();
+            gameObject.SetActive(false);
+        }
+
+        private void GameWon(bool winningPlayer)
+        {
+            TextMeshProUGUI winningText = GetComponentInChildren<TextMeshProUGUI>();
+            if (!winningPlayer == InstanceFinder.GameManager.IsHost)
+            {
+                winningText.text = "You won! Congratulations!";
+            }
+            else 
+            {
+                winningText.text = "You lost. Want to try again?";
+            }
+            gameObject.SetActive(true);
+        }
+    }
+}
