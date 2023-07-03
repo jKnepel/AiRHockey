b09e9605c7d36fd4c882d624590478f43307c169
SelinaWernike <63050357+SelinaWernike@users.noreply.github.com>
2023-07-03 15:19:05 +0200

    Improved UI, added handmenu and fixed rotation

--- Assets/Scripts/Runtime/AiRHockey/UI/PlayerUI.cs
+++ Assets/Scripts/Runtime/AiRHockey/UI/PlayerUI.cs
@@ -9,14 +9,61 @@ namespace HTW.AiRHockey.Game
     public class PlayerUI : MonoBehaviour
     {
 
-        [SerializeField] private TextMeshPro[] timer;
+        [SerializeField] private TMP_Text[] Timers;
 
-        [SerializeField] private TextMeshPro[] Player1Score;
-        [SerializeField] private TextMeshPro[] Player2Score;
+        [SerializeField] private TMP_Text[] Player1Score;
+        [SerializeField] private TMP_Text[] Player2Score;
+
+        private float p1Rotation = 0f;
+        private float p1TimerRotation = -90f;
+        private float pRotation = 180;
+        private bool host = false;
 
         private void Start()
 		{
+            Timers[0].text = "12:99";
             GameManagerEvents.OnGoalScored += OnGoalScored;
+            //Set rotation according to player perspective, but only the ones on the table.
+            if(InstanceFinder.GameManager.IsHost)
+            {
+                foreach(TextMeshPro text in Player2Score)
+                {
+                    if(text.gameObject.tag.Equals("Table"))
+                    {
+                        Vector3 initRotation = text.transform.parent.rotation.eulerAngles;
+                        text.transform.parent.rotation = Quaternion.Euler(initRotation.x, p1Rotation, initRotation.z);
+                    }
+                }
+
+                foreach(TextMeshPro timer in Timers)
+                {
+                    if (timer.gameObject.tag.Equals("Table"))
+                    {
+                        Vector3 initRotation = timer.transform.rotation.eulerAngles;
+                        timer.transform.rotation = Quaternion.Euler(initRotation.x, initRotation.y, p1TimerRotation);
+                    }
+                }
+
+            } else
+            {
+                foreach (TextMeshPro text in Player1Score)
+                {
+                    if (text.gameObject.tag.Equals("Table"))
+                    {
+                        Vector3 initRotation = text.transform.parent.rotation.eulerAngles;
+                        text.transform.parent.rotation = Quaternion.Euler(initRotation.x, p1Rotation + pRotation, initRotation.z);
+                    }
+                }
+
+                foreach (TextMeshPro timer in Timers)
+                {
+                    if (timer.gameObject.tag.Equals("Table"))
+                    {
+                        Vector3 initRotation = timer.transform.rotation.eulerAngles;
+                        timer.transform.rotation = Quaternion.Euler(initRotation.x, p1TimerRotation + pRotation, initRotation.z);
+                    }
+                }
+            }
 		}
 
         private void Update()
@@ -38,10 +85,35 @@ namespace HTW.AiRHockey.Game
 
         public void UpdateTimer()
         {
-            foreach(TextMeshPro textMesh in timer)
+            foreach(TextMeshPro textMesh in Timers)
             {
                 textMesh.text = InstanceFinder.GameManager.GameTimeText;
             }
+        }
+
+        public void DisplayOptions()
+        {
+            Debug.Log("Display Options");
+        }
+
+        public void HideOptions()
+        {
+            Debug.Log("Hide Options");
+        }
+
+        public void BackToMenu()
+        {
+            InstanceFinder.GameManager.DisconnectFromServer();
+        }
+
+        public void ResetPlayer()
+        {
+            InstanceFinder.GameManager.ResetPlayers();
+        }
+
+        public void Rematch()
+        {
+            InstanceFinder.GameManager.EndGame();
         }
     }
 }
